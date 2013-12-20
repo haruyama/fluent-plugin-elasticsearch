3cd4e9e0c6b4bf1bd1ad8bfe69fb69d445f98639
HARUYAMA Seigo <haruyama@unixuser.org>
2013-12-20 17:30:57 +0900

    release v0.0.1

---
+++ lib/fluent/plugin/out_solr.rb
@@ -0,0 +1,50 @@
+# encoding: UTF-8
+require 'net/http'
+require 'uri'
+
+# Solr output plugin for Fluent
+class Fluent::SolrOutput < Fluent::BufferedOutput
+  Fluent::Plugin.register_output('solr', self)
+
+  config_param :host,       :string,  default: 'localhost'
+  config_param :port,       :integer, default: 8983
+  config_param :core,       :string,  default: 'collection1'
+
+  include Fluent::SetTagKeyMixin
+  config_set_default :include_tag_key, false
+
+  def initialize
+    super
+  end
+
+  def configure(conf)
+    super
+  end
+
+  def start
+    super
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def shutdown
+    super
+  end
+
+  def write(chunk)
+    bulk_message = []
+
+    chunk.msgpack_each do |tag, time, record|
+      record.merge!(@tag_key => tag) if @include_tag_key
+
+      bulk_message << record
+    end
+
+    http = Net::HTTP.new(@host, @port.to_i)
+    request = Net::HTTP::Post.new('/solr/' + URI.escape(@core) + '/update', 'content-type' => 'application/json; charset=utf-8')
+    request.body = Yajl::Encoder.encode(bulk_message)
+    http.request(request).value
+  end
+end
---
+++ test/plugin/test_out_solr.rb
@@ -0,0 +1,116 @@
+require 'test/unit'
+
+require 'fluent/test'
+require 'fluent/plugin/out_solr'
+
+require 'webmock/test_unit'
+require 'date'
+
+require 'helper'
+
+$LOAD_PATH.push File.expand_path('../lib', __FILE__)
+$LOAD_PATH.push File.dirname(__FILE__)
+
+WebMock.disable_net_connect!
+
+# Solr output test
+class SolrOutput < Test::Unit::TestCase
+  attr_accessor :index_cmds, :content_type
+
+  def setup
+    Fluent::Test.setup
+    @driver = nil
+  end
+
+  def driver(tag = 'test', conf = '')
+    @driver ||= Fluent::Test::BufferedOutputTestDriver.new(Fluent::SolrOutput, tag).configure(conf)
+  end
+
+  def sample_record
+    { 'age' => 26, 'request_id' => '42' }
+  end
+
+  def stub_solr(url = 'http://localhost:8983/solr/collection1/update')
+    stub_request(:post, url).with do |req|
+      @content_type = req.headers['Content-Type']
+      @index_cmds = JSON.parse(req.body)
+    end
+  end
+
+  def stub_solr_unavailable(url = 'http://localhost:8983/solr/collection1/update')
+    stub_request(:post, url).to_return(status: [503, 'Service Unavailable'])
+  end
+
+  def test_writes_to_default_index
+    stub_solr
+    driver.emit(sample_record)
+    driver.run
+    assert_equal(26,   @index_cmds[0]['age'])
+    assert_equal('42', @index_cmds[0]['request_id'])
+  end
+
+  def test_wrties_with_proper_content_type
+    stub_solr
+    driver.emit(sample_record)
+    driver.run
+    assert_equal('application/json; charset=utf-8', @content_type)
+  end
+
+  def test_writes_to_speficied_core
+    driver.configure("core mycore\n")
+    solr_request = stub_solr('http://localhost:8983/solr/mycore/update')
+    driver.emit(sample_record)
+    driver.run
+    assert_requested(solr_request)
+  end
+
+  def test_writes_to_speficied_host
+    driver.configure("host 192.168.33.50\n")
+    solr_request = stub_solr('http://192.168.33.50:8983/solr/collection1/update')
+    driver.emit(sample_record)
+    driver.run
+    assert_requested(solr_request)
+  end
+
+  def test_writes_to_speficied_port
+    driver.configure("port 9201\n")
+    solr_request = stub_solr('http://localhost:9201/solr/collection1/update')
+    driver.emit(sample_record)
+    driver.run
+    assert_requested(solr_request)
+  end
+
+  def test_makebulk
+    stub_solr
+    driver.emit(sample_record)
+    driver.emit(sample_record.merge('age' => 27))
+    driver.run
+    assert_equal(2, index_cmds.count)
+    assert_equal(26, @index_cmds[0]['age'])
+    assert_equal(27, @index_cmds[1]['age'])
+  end
+
+  def test_doesnt_add_tag_key_by_default
+    stub_solr
+    driver.emit(sample_record)
+    driver.run
+    assert_nil(index_cmds[0]['tag'])
+  end
+
+  def test_adds_tag_key_when_configured
+    driver('mytag').configure("include_tag_key true\n")
+    stub_solr
+    driver.emit(sample_record)
+    driver.run
+    assert(index_cmds[0].key?('tag'))
+    assert_equal(index_cmds[0]['tag'], 'mytag')
+  end
+
+  def test_request_error
+    stub_solr_unavailable
+    driver.emit(sample_record)
+    assert_raise(Net::HTTPFatalError) do
+      driver.run
+    end
+  end
+end
