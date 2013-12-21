971bf360ce4d870e77bdfd97285a42dd51bf0e65
HARUYAMA Seigo <haruyama@unixuser.org>
2013-12-21 18:19:49 +0900

    introduce SolrUtil

--- lib/fluent/plugin/out_solr.rb
+++ lib/fluent/plugin/out_solr.rb
@@ -4,6 +4,9 @@
 class Fluent::SolrOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('solr', self)
 
+  require 'fluent/plugin/solr_util'
+  include SolrUtil
+
   config_param :host,              :string,  default: 'localhost'
   config_param :port,              :integer, default: 8983
   config_param :core,              :string,  default: 'collection1'
@@ -43,19 +46,6 @@ class Fluent::SolrOutput < Fluent::BufferedOutput
   end
 
   def write(chunk)
-    documents = []
-
-    chunk.msgpack_each do |tag, unixtime, record|
-      time = Time.at(unixtime)
-      time = time.utc unless @localtime
-      record.merge!(@time_field => time.strftime('%FT%TZ'))
-      record.merge!(@tag_key    => tag) if @include_tag_key
-      documents << record
-    end
-
-    http = Net::HTTP.new(@host, @port.to_i)
-    request = Net::HTTP::Post.new('/solr/' + URI.escape(@core) + '/update', 'content-type' => 'application/json; charset=utf-8')
-    request.body = Yajl::Encoder.encode(documents)
-    http.request(request).value
+    update_core(chunk, @core)
   end
 end
