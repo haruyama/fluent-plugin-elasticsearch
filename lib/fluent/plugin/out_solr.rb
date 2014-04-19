892ceda32824222feb6b91d123e304b55dfde1a4
HARUYAMA Seigo <haruyama@unixuser.org>
2014-04-19 16:54:29 +0900

    Suppress rubocop warnings.

--- lib/fluent/plugin/out_solr.rb
+++ lib/fluent/plugin/out_solr.rb
@@ -1,46 +1,48 @@
 # encoding: UTF-8
 
-# Solr output plugin for Fluent
-class Fluent::SolrOutput < Fluent::BufferedOutput
-  Fluent::Plugin.register_output('solr', self)
+module Fluent
+  # Solr output plugin for Fluent
+  class SolrOutput < Fluent::BufferedOutput
+    Fluent::Plugin.register_output('solr', self)
 
-  require 'fluent/plugin/solr_util'
-  include SolrUtil
-  require 'fluent/plugin/solr_config_common'
-  include SolrConfigCommon
+    require 'fluent/plugin/solr_util'
+    include SolrUtil
+    require 'fluent/plugin/solr_config_common'
+    include SolrConfigCommon
 
-  config_param :core, :string,  default: 'collection1'
+    config_param :core, :string,  default: 'collection1'
 
-  def initialize
-    require 'net/http'
-    require 'uri'
-    require 'time'
-    super
-    @localtime = true
-  end
-
-  def configure(conf)
-    if conf['utc']
-      @localtime = false
-    elsif conf['localtime']
+    def initialize
+      require 'net/http'
+      require 'uri'
+      require 'time'
+      super
       @localtime = true
     end
-    super
-  end
 
-  def start
-    super
-  end
+    def configure(conf)
+      if conf['utc']
+        @localtime = false
+      elsif conf['localtime']
+        @localtime = true
+      end
+      super
+    end
 
-  def format(tag, time, record)
-    [tag, time, record].to_msgpack
-  end
+    def start
+      super
+    end
 
-  def shutdown
-    super
-  end
+    def format(tag, time, record)
+      [tag, time, record].to_msgpack
+    end
 
-  def write(chunk)
-    update_core(chunk, @core)
+    def shutdown
+      super
+    end
+
+    def write(chunk)
+      update_core(chunk, @core)
+    end
   end
 end
